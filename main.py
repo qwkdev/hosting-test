e35b4634c8627e60d4f3ab1a21f671d104471dc2
qwk <80052118+qwkdev@users.noreply.github.com>
2024-03-30 14:29:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from flask import Flask
 import json
 from datetime import datetime
 
-app = Flask('test')
+app = Flask(__name__)
 
-with open('data.json') as f:
+with open('site/data.json') as f:
     x = json.load(f)
 
 @app.route('/')
 def index():
+    with open('site/data.json') as f:
+        x = json.load(f)
     return x
 
 @app.route('/update')
 def update():
     x = {'time': datetime.now().strftime("%H:%M:%S")}
-    with open('data.json', 'w') as f:
+    with open('site/data.json', 'w') as f:
         json.dump(x, f)
-            
-app.run()
